7aab320bf1862f98690f17e49f882c0e54768884
areyou4real <119920294+areyou4real@users.noreply.github.com>
2025-08-13 15:10:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ with st.container():
     with r1c1:
         age_now = st.number_input("Current age", min_value=16, max_value=80, value=25, step=1)
     with r1c2:
-        age_retire = st.number_input("Target retirement age", min_value=age_now+1, max_value=90, value=60, step=1)
+        age_retire = st.number_input("Target retirement age", min_value=age_now+1, max_value=80, value=60, step=1)
     with r1c3:
         life_expectancy = st.number_input("Life expectancy", min_value=age_retire+1, max_value=110, value=90, step=1)
 
